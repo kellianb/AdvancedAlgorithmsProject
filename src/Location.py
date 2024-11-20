a4c9c10c762a0e70f5303c68fb748357ea4edfde
kellianb <kellian.bechtel@hotmail.com>
2024-11-20 16:22:58 +0100

    Added function to determine the closest location whose delivery window is reachable

--- src/Location.py
+++ src/Location.py
@@ -1,6 +1,9 @@
+from copy import deepcopy
 from math import sqrt
 from dataclasses import dataclass
 from operator import itemgetter
+from typing import Optional
+
 
 @dataclass
 class Location:
@@ -46,3 +49,26 @@ class Location:
         others = [pair[1] for pair in distance_pairs]
 
         return closest, others
+
+    def find_nearest_reachable(self, others: list["Location"], current_cost: int = 0) -> tuple[
+        Optional["Location"], float, list["Location"]]:
+        """Then find the neighbor that is the cheapest to deliver to from the current location, return it and the remaining list.
+
+            If no neighbor is found, return None.
+
+            :arg others: list of locations to check
+            :arg current_cost: Cost that was already incurred before reaching this location
+            :return: Closest neighbor to the current location, The Cost to get to this neighbor and the remaining list.
+            """
+
+        # Remove elements that cannot be reached
+        others_reachable = [loc for loc in deepcopy(others) if loc.due_date >= self.distance_to(loc) + current_cost]
+
+        if not others_reachable:
+            return None, current_cost, others
+
+        cheapest = min(others_reachable, key=lambda x: self.cost_to(x, current_cost))
+
+        others.remove(cheapest)
+
+        return cheapest, self.cost_to(cheapest, current_cost), others
