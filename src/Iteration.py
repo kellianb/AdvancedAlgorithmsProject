e1c7f3d89c3cfd88f70603aeb05816d80ed243f1
kellianb <kellian.bechtel@hotmail.com>
2024-11-22 14:40:17 +0100

    Implements ACO in Iteration - Implemented the Aco Heuristic in the Iteration dataclass

--- src/Iteration.py
+++ src/Iteration.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 from src.Location import Location
 from src.Route import Route
+import src.Heuristics.AntColony as AntColony
+from typing import Optional
+from multiprocessing import Pool
+
+
 
 
 @dataclass
@@ -58,6 +63,88 @@ class Iteration:
 
             # Append the route to the list of routes
             self.routes.append(route)
+        return self
+
+    def aco_heuristic(self, n_ants: int, max_iter: int, alpha: int, beta: int, rho: float) -> "Iteration":
+        """Generate VRP routes using the ACO heuristic
+            :param n_ants: Number of ants to use
+            :param max_iter: Maximum number of iterations
+            :param alpha: Alpha parameter, controls the influence of pheromones
+            :param beta: Beta parameter, controls the influence of cost
+            :param rho: Rho parameter, controls the pheromone evaporation rate
+        """
+        # Pheromone matrix : (a : b) -> pheromone level from location a to b
+        pheromones = {}
+        best_cost = float("inf")
+        best_solution = None
+
+        for a in self._locationBuf + [self.warehouse]:
+            for b in self._locationBuf + [self.warehouse]:
+                if b != a:
+                    pheromones[(a, b)] = 1
+
+        # Run max_iter iterations
+        with Pool() as pool:
+            for _ in range(max_iter):
+                # Generate solutions concurrently
+                solutions = pool.starmap(AntColony.construct_solution, [(alpha, beta, self._locationBuf, self.warehouse, self.vehicleCapacity, pheromones) for _ in range(n_ants)])
+
+                self._aco_heuristic_update_pheromones(solutions, rho, pheromones)
+
+                # Find the best solution
+                for solution in solutions:
+                    cost = self.total_cost(solution)
+
+                    if cost < best_cost:
+                        best_cost = cost
+                        best_solution = solution
+
+        self._locationBuf = []
+        self.routes = best_solution
+
+        return self
+
+    def _aco_heuristic_update_pheromones(self, solutions: list[list[Route]], rho: float, pheromones: dict):
+        """Update the pheromone levels based on the routes taken
+            :arg routes: List of routes taken
+            :arg rho: Rho parameter, controls the pheromone evaporation rate
+            :arg pheromones: Pheromone matrix
+        """
+        # Evaporate pheromones
+        for key in pheromones.keys():
+            pheromones[key] *= (1 - rho)
+
+        # Add pheromones to the edges taken
+        for solution in solutions:
+            deposit = 1 / self.total_cost(solution)
+
+            for route in solution:
+                # Add pheromones for the edges between the warehouse and the customers
+                pheromones[(self.warehouse, route.customers[0])] += deposit
+
+                # Add the pheromones for the edges between the customers
+                for i in range(len(route.customers) - 1):
+                    pheromones[(route.customers[i], route.customers[i + 1])] += deposit
+
+                # Add pheromones for the edge between the last customer and the warehouse
+                pheromones[(route.customers[-1], self.warehouse)] += deposit
+
+    def total_cost(self, routes: Optional[list[Route]] = None) -> float:
+        """Calculate the total cost of all routes
+        :arg routes: (Optional) List of routes to calculate the cost for, if left empty, the routes in the iteration are used"""
+        routes = routes if routes else self.routes
+        return sum([route.cost() for route in routes])
+
+    def print(self, routes: Optional[list[Route]] = None) -> "Iteration":
+        """Print the details of the iteration"""
+        routes = routes if routes else self.routes
+        print("==== Iteration =====")
+        print(f"Total cost: {self.total_cost(routes)}")
+        print(f"Total routes: {len(routes)}")
+        print("\n")
+        for i, route in enumerate(routes):
+            route.print(name=f"Route {i + 1}")
+            print("")
         return self
 
     def plot(self) -> "Iteration":
